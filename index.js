0dd118b2201397bbcc7e06037c4bab9466d21fa8
alaminjim <jimalamin7@gmail.com>
2025-05-12 18:41:16 +0600

    added edit 2

--- index.js
+++ index.js
@@ -93,11 +93,6 @@ async function run() {
       const result = await coffeeCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
-
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
